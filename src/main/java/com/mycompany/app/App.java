c5a708639ef02d10bc03078c5ba60cdd57192a63
Harsika Agarwal <76247860+Harsika-dev@users.noreply.github.com>
2023-07-09 23:50:36 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int sum = CalculatorModule1.add(a, b);
+        
+        System.out.println("Sum: " + sum);
     }
 }
